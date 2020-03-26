f9b4219553bbebc2ad989217d51519b20f60dcbb
Germain Lemasson <germain.lemasson@gmail.com>
2020-03-26 09:11:10 +0100

    Restructure DataHandler and add OutputsHandler

--- monitor/datahandler.py
+++ monitor/datahandler.py
@@ -7,23 +7,24 @@ class DataInputManager:
         self.ymin,self.ymax=data_range
         self.arraysize=arraysize
         self.data=np.arange(self.ymin,self.ymax, (self.ymax-self.ymin)/self.arraysize)
+    def get_range(self):
+        return (self.ymin,self.ymax)
 
-class DataHandler(Thread):
-
+class DataInputs:
     def __init__(self, xmax, freq, pressure_range, flow_range, volume_range):
         Thread.__init__(self)
         self.running=True
         self.index=0
         self.index_zero_time = 0
-        
+
         self.xmax=xmax
         self.freq=freq
         self.arraysize=xmax*freq
 
-        self.pressure_manager=DataInputManager(self.arraysize, pressure_range)
-        self.flow_manager=DataInputManager(self.arraysize, flow_range)
-        self.volume_manager=DataInputManager(self.arraysize, volume_range)
-    
+        self.pressure=DataInputManager(self.arraysize, pressure_range)
+        self.flow=DataInputManager(self.arraysize, flow_range)
+        self.volume=DataInputManager(self.arraysize, volume_range)
+
     def get_index(self):
         return self.index
 
@@ -34,25 +35,52 @@ class DataHandler(Thread):
         else:
             diff=timestamp-self.index_zero_time
             self.index=int(diff*self.freq)
+
+class DataOutputManager:
+
+    def __init__(self, vmin=0, vmax=100, default=0):
+        self.vmin=vmin
+        self.vmax=vmax
+        self.value=default
+
+    def update(self,value):
+        self.value=value
+
+
+class DataOutputs:
+
+    def __init__(self):
+        self.fio2=DataOutputManager(default=22)
+        self.pep=DataOutputManager(default=10)
+        self.fr=DataOutputManager(default=22)
+        self.flow=DataOutputManager(0.0,30.0,6.0)
+        self.vt=DataOutputManager(0,1000,370)
+
+class DataHandler(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.inputs=None
+        self.outputs=DataOutputs()
+        self.running=False
+
     def stop(self):
         self.running=False
 
 class DataHandlerDummy(DataHandler):
 
-    def __init__(self, xmax, freq, pressure_range, flow_range, volume_range):
-        DataHandler.__init__(self, xmax, freq, pressure_range, flow_range, volume_range)
-
     def run(self):
 
+        self.running=True
         while self.running:
-            self.make_index(time.time())
+            self.inputs.make_index(time.time())
             time.sleep(1.0/40)
             v = np.random.rand(4)
             if v[3] > 0.03:
-                self.pressure_manager.data[self.index]=0
-                self.flow_manager.data[self.index]=0
-                self.volume_manager.data[self.index]=0
+                self.inputs.pressure.data[self.inputs.index]=0
+                self.inputs.flow.data[self.inputs.index]=0
+                self.inputs.volume.data[self.inputs.index]=0
             else:
-                self.pressure_manager.data[self.index]=self.pressure_manager.ymax*v[0]
-                self.flow_manager.data[self.index]=self.flow_manager.ymax*v[1]
-                self.volume_manager.data[self.index]=self.volume_manager.ymax*v[2]
+                self.inputs.pressure.data[self.inputs.index]=self.inputs.pressure.ymax*v[0]
+                self.inputs.flow.data[self.inputs.index]=self.inputs.flow.ymax*v[1]
+                self.inputs.volume.data[self.inputs.index]=self.inputs.volume.ymax*v[2]
--- monitor/window.py
+++ monitor/window.py
@@ -4,26 +4,26 @@ import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-from .datahandler import DataHandlerDummy
+from .datahandler import DataHandlerDummy, DataInputs
 
 
 class Scope:
-    def __init__(self, ax, title, ylabel, xlim, xstep, ylim, handler):
+    def __init__(self, ax, title, ylabel, xlim, xstep, handler):
 
+        self.handler=handler
         self.ax=ax
         self.ax.set_title(title,loc='left')
         self.ax.set_ylabel(ylabel)
         self.ax.set_xlim(xlim)
-        self.ax.set_ylim(ylim)
-        self.handler=handler
+        self.ax.set_ylim(self.handler.get_range())
 
         self.tdata=np.arange(xlim[0], xlim[1], xstep)
-        
+
         self.line_a = matplotlib.lines.Line2D(self.tdata, self.handler.data)
         self.line_b = matplotlib.lines.Line2D(self.tdata, self.handler.data)
         self.ax.add_line(self.line_a)
         self.ax.add_line(self.line_b)
-    
+
     def update(self, index):
         self.iterator=index
         self.line_a.set_data(self.tdata[0:self.iterator], self.handler.data[0:self.iterator])
@@ -33,61 +33,63 @@ class Scope:
         else:
             indx_b = len(self.tdata)-1
         self.line_b.set_data(self.tdata[indx_b:len(self.tdata)-1], self.handler.data[indx_b:len(self.tdata)-1])
-        
-        return self.line_a, self.line_b,
- 
 
+        return self.line_a, self.line_b,
+
+class RangeSetter:
+    def __init__(self, app, value, label, handler):
+        self.handler=handler
+        self.scale=tk.Scale(app, variable=value, label=label, from_=handler.vmin, to=handler.vmax, command=self.update)
+        self.scale.set(handler.value)
+
+    def update(self,value):
+        self.handler.update(value)
 
 class Window:
 
     def __init__(self):
-        
+
         self.timewindow=15
         self.freq=20
         self.timeresolution=1.0/self.freq
 
-        
-        
+
+
         self.app = tk.Tk()
         self.app.wm_title("Graphe Matplotlib dans Tkinter")
         tk.Grid.rowconfigure(self.app, 0, weight=1)
         tk.Grid.columnconfigure(self.app, 0, weight=1)
         tk.Button(self.app, text='Quitter', command=self.app.quit).grid(row=3,column=0)
         #Graph Init 
-        self.data_handler = DataHandlerDummy(self.timewindow,self.freq,(-30,105),(-100,100),(0,500))
-        
+        self.data_handler = DataHandlerDummy()
+        self.data_handler.inputs=DataInputs(self.timewindow,self.freq,(-30,105),(-100,100),(0,500))
+
         self.fig_graph, (self.ax_pressure, self.ax_flow, self.ax_volume) = plt.subplots(3, 1)
         self.xlim=(0,self.timewindow)
-        self.scope_pressure=Scope(self.ax_pressure,"Pression","mBar",self.xlim, self.timeresolution, (-30,105), self.data_handler.pressure_manager)
-        self.scope_flow=Scope(self.ax_flow,"Débit","L/min",self.xlim, self.timeresolution, (-100,100), self.data_handler.flow_manager)
-        self.scope_volume=Scope(self.ax_volume,"Volume","mL",self.xlim, self.timeresolution, (0,500), self.data_handler.volume_manager)
+        self.scope_pressure=Scope(self.ax_pressure,"Pression","mBar",self.xlim, self.timeresolution, self.data_handler.inputs.pressure)
+        self.scope_flow=Scope(self.ax_flow,"Débit","L/min",self.xlim, self.timeresolution, self.data_handler.inputs.flow)
+        self.scope_volume=Scope(self.ax_volume,"Volume","mL",self.xlim, self.timeresolution, self.data_handler.inputs.volume)
         self.canvas_graph = FigureCanvasTkAgg(self.fig_graph, self.app)
         self.canvas_graph.get_tk_widget().grid(row=0, column=0, rowspan=3, sticky=tk.N+tk.S+tk.E+tk.W)
         matplotlib.animation.FuncAnimation(self.fig_graph, self.update, interval=self.timeresolution,blit=True)
 
-        value = tk.IntVar()
-        self.scale = tk.Scale(self.app, variable=value, label="FiO2")
-        self.scale.grid(row=0, column=1)
+        self.range_fio2=RangeSetter(self.app, tk.IntVar(), "FiO2", self.data_handler.outputs.fio2)
+        self.range_fio2.scale.grid(row=0, column=1)
 
-        value = tk.IntVar()
-        self.scale = tk.Scale(self.app, variable=value, label="PEP")
-        self.scale.grid(row=0, column=2)
+        self.range_pep=RangeSetter(self.app, tk.IntVar(), "PEP", self.data_handler.outputs.pep)
+        self.range_pep.scale.grid(row=0, column=2)
 
-        value = tk.IntVar()
-        self.scale = tk.Scale(self.app, variable=value, label="FR")
-        self.scale.grid(row=1, column=1)
+        self.range_fr=RangeSetter(self.app, tk.IntVar(), "FR", self.data_handler.outputs.fr)
+        self.range_fr.scale.grid(row=1, column=1)
 
-        value = tk.IntVar()
-        self.scale = tk.Scale(self.app, variable=value, label="VM")
-        self.scale.grid(row=1, column=2)
+        self.range_flow=RangeSetter(self.app, tk.IntVar(), "Debit", self.data_handler.outputs.flow)
+        self.range_flow.scale.grid(row=1, column=2)
 
-        value = tk.IntVar()
-        self.scale = tk.Scale(self.app, variable=value, label="VTe")
-        self.scale.grid(row=2, column=1)
-
+        self.range_vt=RangeSetter(self.app, tk.IntVar(), "VT", self.data_handler.outputs.vt)
+        self.range_vt.scale.grid(row=2, column=1)
 
     def update(self, frame):
-        index = self.data_handler.get_index()
+        index = self.data_handler.inputs.get_index()
         sp_in_a,sp_in_b = self.scope_pressure.update(index)
         sp_fl_a,sp_fl_b = self.scope_flow.update(index)
         sp_vl_a,sp_vl_b = self.scope_volume.update(index)
