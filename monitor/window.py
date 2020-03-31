74906dcf5e34baa6f1f3593857a7244b711ba9b6
Vivien Delmon <vivien.delmon@minmaxmedical.com>
2020-03-31 13:49:10 +0200

    FuncAnimation require ms interval not second
    This fixes the high CPU usage

--- monitor/window.py
+++ monitor/window.py
@@ -208,7 +208,7 @@ class Window:
         
         self.canvas_graph = FigureCanvasTkAgg(self.fig_graph, self.app)
         self.canvas_graph.get_tk_widget().grid(row=1, column=0, rowspan=4,columnspan=8, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.animation = matplotlib.animation.FuncAnimation(self.fig_graph, self.update, interval=self.timeresolution,blit=True)
+        self.animation = matplotlib.animation.FuncAnimation(self.fig_graph, self.update, interval=self.timeresolution * 1000,blit=True)
 
         self.freeze_time=False
         self.delta_marker=None
