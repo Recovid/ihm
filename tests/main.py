4a392e5bb243deda76233679083625af135c3190
Vivien Delmon <vivien.delmon@minmaxmedical.com>
2020-04-04 12:24:16 +0200

    Add cyclic RespMsg in nominal_cycle generation

--- tests/main.py
+++ tests/main.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
         sys.exit(1)
     fr_pm = 25
     fr_hz = fr_pm / 60 # sec
+    period_ms = 1000 / fr_hz
+    period_nb = 1
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     with io.open('nominal_cycle.txt','w') as nominal_cycle:
         nominal_cycle.write(serialize_msg(SetMsg(Data.VT, 350)))
@@ -33,8 +35,15 @@ if __name__ == '__main__':
         nominal_cycle.write(serialize_msg(SetMsg(Data.PEP, 6)))
         nominal_cycle.write(serialize_msg(SetMsg(Data.FLOW,58)))
         nominal_cycle.write(serialize_msg(SetMsg(Data.TPLAT,200)))
+        nominal_cycle.write(serialize_msg(SetMsg(Data.PMAX,60)))
+        nominal_cycle.write(serialize_msg(SetMsg(Data.PMIN,20)))
+        nominal_cycle.write(serialize_msg(SetMsg(Data.VTMIN,400)))
+        nominal_cycle.write(serialize_msg(SetMsg(Data.VMMIN,5)))
         nominal_cycle.write(serialize_msg(resp_msg(fr_hz)))
         for t_ms in range(0, int(sys.argv[1]), int(1000/fr_pm)):
+            if t_ms / period_ms > period_nb:
+                period_nb += 1
+                nominal_cycle.write(serialize_msg(resp_msg(fr_hz)))
             nominal_cycle.write(serialize_msg(nominal_data(t_ms, fr_hz)))
         nominal_cycle.write(serialize_msg(SetMsg(Data.VT, 350)))
         nominal_cycle.write(serialize_msg(SetMsg(Data.FR, 19)))
