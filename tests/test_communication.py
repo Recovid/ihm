fe95192fc15db91a7a16a4b8fb4e78e5c66f11a2
Yoann Blein <yoann.blein@free.fr>
2020-04-09 12:08:11 +0200

    Use `assertIsNone` instead of `assertEqual` to None

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -9,23 +9,23 @@ class TestCommonMsgStructure(unittest.TestCase):
 
     def test_missing_type(self):
         frame = "04400 Vol__:0000 Deb__:+025 Paw__:+020\tCS8:6F\n"
-        self.assertEqual(parse_msg(frame), None)
+        self.assertIsNone(parse_msg(frame))
 
     def test_missing_type_with_crc_collision(self):
         frame = "04400 Vol__:0000 Deb__:+025 Paw__:+020\tCS8:F4\n"
-        self.assertEqual(parse_msg(frame), None)
+        self.assertIsNone(parse_msg(frame))
 
     def test_missing_crc_value(self):
         frame = "DATA msec_:04280 Vol__:0000 Deb__:+010 Paw__:+008\tCS"
-        self.assertEqual(parse_msg(frame), None)
+        self.assertIsNone(parse_msg(frame))
 
     def test_wrong_crc(self):
         frame = "DATA msec_:04280 Vol__:0000 Deb__:+010 Paw__:+008\tCS8:73\n"
-        self.assertEqual(parse_msg(frame), None)
+        self.assertIsNone(parse_msg(frame))
 
     def test_incomplete_msg(self):
         frame = "DAT\tCS8:6F\n"
-        self.assertEqual(parse_msg(frame), None)
+        self.assertIsNone(parse_msg(frame))
 
 class TestDataMsg(unittest.TestCase):
 
